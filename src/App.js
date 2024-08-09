d399b8492b9d6ef65d5e7bd51effe710ce97c617
Laxical <vinnu24.vinay@gmail.com>
2024-08-10 01:28:59 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import {useState, useEffect, useRef} from "react"
 import {Routes, Route} from "react-router-dom"
+import { ethers } from "ethers"
 import User from "./components/User.jsx"
 import Verifier from "./components/Verifier.jsx"
 import Admin from "./components/Admin.jsx"
-import Error from "./components/Error.jsx"
+import Bluexross from "./artifacts/contracts/Bluexross.sol/Bluexross.json"
+import StakeTokens from "./artifacts/contracts/stakecoin.sol/StakeTokens.json"
 
 const blueAddress = "0x40fC681E876a143Ec7358E74f75a9ED717B1fec1";
 const stakeAddress = "0xB961fAc6a3Ae50661C8288dC3a5B5D5903CfD41B";
 const rewardAddress = "0x9F16BE65Afb4a2Bb16EDCbA788708D590aA9cfa2";
 
 function App() {
+
   const [ckverifer,setCkverifier]=useState(false);
   const [ckowner,setOwner]=useState(false);
 
@@ -29,19 +32,13 @@ function App() {
     setOwner(transaction);
   }
 
-  useEffect(() => {
-    ckVerifyer();
-    ckAdmin();
-  },[])
-
   return(
     <>
       <Routes>
         <Route path="/" element={<User blueAddress = {blueAddress} stakeAddress={stakeAddress} rewardAddress={rewardAddress}/>}/>
         <Route path="/user" element={<User blueAddress = {blueAddress} stakeAddress={stakeAddress} rewardAddress={rewardAddress}/>}/>
-        <ProtectedRoute isAuthenticated={ckverifer} path="/verifier" element={<Verifier blueAddress = {blueAddress} stakeAddress={stakeAddress} rewardAddress={rewardAddress}/>}/>
-        <ProtectedRoute isAuthenticated={ckowner} path="/admin" element={<Admin blueAddress = {blueAddress} stakeAddress={stakeAddress} rewardAddress={rewardAddress}/>}/>
-        <Route path="/user" element={<Error/>}/>
+        <Route path="/verifier" element={<Verifier blueAddress = {blueAddress} stakeAddress={stakeAddress} rewardAddress={rewardAddress}/>}/>
+        <Route path="/admin" element={<Admin blueAddress = {blueAddress} stakeAddress={stakeAddress} rewardAddress={rewardAddress}/>}/>
       </Routes>
     </>
   )
